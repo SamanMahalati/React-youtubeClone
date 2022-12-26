1db5ff6feb1f2f24cfc99e51f6ab629db2e39c2d
Saman Mahalati <mahalatisaman@gmail.com>
2022-12-26 16:15:58 +0330

    import all icon to constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,32 @@
+import LogoImg from "../images/LOGO.png"
+
+//Import Icon 
+import PodcastsIcon from '@mui/icons-material/Podcasts';
+import CodeIcon from '@mui/icons-material/Code';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import SchoolIcon from '@mui/icons-material/School';
+import SlideshowIcon from '@mui/icons-material/Slideshow';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+
+//Logo
+export const Logo = LogoImg
+
+//Categories List
+export const Categories = [
+    {name: "New" , icon: <HomeIcon/> } ,
+    {name: "Coding" , icon: <CodeIcon/> } ,
+    {name: "React Js" , icon: <CodeIcon/> } ,
+    {name: "Next Js" , icon: <CodeIcon/> } ,
+    {name: "Music" , icon: <MusicNoteIcon/> } ,
+    {name: "Education" , icon: <SchoolIcon/> } ,
+    {name: "Podcast" , icon: <PodcastsIcon/> } ,
+    {name: "Movie" , icon: <SlideshowIcon/> } ,
+    {name: "Gaming" , icon: <SportsEsportsIcon/> } ,
+    {name: "Fitness" , icon: <FitnessCenterIcon/> } ,
+]
+
