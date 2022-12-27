1b11dc6f22e6e138e3807522b8824ec332a53e63
Saman Mahalati <mahalatisaman@gmail.com>
2022-12-28 00:05:15 +0330

    Handle search box at navbar for search videos

--- src/shared/SearchBox.jsx
+++ src/shared/SearchBox.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { useDispatch } from "react-redux"
 
+import fetchChannelVideos from '../redux/channelVideos/channelVideosAction';
+
+//Icons
 import SearchIcon from '@mui/icons-material/Search';
 
 //Styles
@@ -12,6 +16,12 @@ const Container = styled.div`
     border-radius: 3rem;
     padding: 0 1rem;
     box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+    div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+    }
 `
 
 const SearchInput = styled.input`
@@ -24,10 +34,19 @@ const SearchInput = styled.input`
 `
 
 const SearchBox = () => {
+    const dispatch = useDispatch()
+    const [search, setSearch] = useState("")
+
+    const SearchHandler = () => {
+        dispatch(fetchChannelVideos(search))
+    }
+
     return (
         <Container>
-            <SearchInput type="text" placeholder='Search...'/>
-            <SearchIcon sx={{color: "#CA3E47"}}/>
+            <SearchInput onChange={event => setSearch(event.target.value)} on value={search} type="text" placeholder='Search...' />
+            <div onClick={SearchHandler}>
+                <SearchIcon sx={{ color: "#CA3E47" }} />
+            </div>
         </Container>
     );
 };
